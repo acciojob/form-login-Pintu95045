ac758eed8ade0aa51baa916a1c9db739ff714a32
Pintu Kumar Giri <131274412+Pintu95045@users.noreply.github.com>
2024-08-24 17:38:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
+	const form=document.getElementById("form1");
+	const formData=new formData(form);
+	const firstName=formData.get('fname');
+	const lastName=formData.get('lname');
 
+		alert(`${firstName} ${lastName}`);
 }
